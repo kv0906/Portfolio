cf0704b0a2bd022954b305537180e19251daeddf
Van Tran <s3515272@rmit.edu.vn>
2016-11-30 09:13:32 +0700

    Update animation for Main-esection

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+< script >
+    $(function () {
+        $(".main-heading").typed({
+            strings: ["I'm Van", "I'm fresh front-end developer", "I'm self-learner"],
+            typeSpeed: 0,
+            showCursor: true,
+            // character for cursor
+            cursorChar: "|"
+        });
+    });
+
+< /script>
